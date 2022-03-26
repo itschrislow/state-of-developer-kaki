23b05b9bade8786ebd65aa8b893a31a94cf78480
chris0610 <chrislow360@gmail.com>
2022-03-25 22:12:03 -0400

    add yoe bar chart component

--- src/lib/nivo.js
+++ src/lib/nivo.js
@@ -1,71 +1,9 @@
 module.exports = {
   textColor: "#f3f4f6",
   fontFamily: "monospace",
-  axis: {
-    domain: {
-      line: {
-        stroke: "#777777",
-        strokeWidth: 1,
-      },
-    },
-    legend: {
-      text: {
-        fill: "#333333",
-      },
-    },
-    ticks: {
-      line: {
-        stroke: "#777777",
-        strokeWidth: 1,
-      },
-      text: {
-        fill: "#f3f4f6",
-      },
-    },
-  },
-  grid: {
-    line: {
-      stroke: "#dddddd",
-      strokeWidth: 1,
-    },
-  },
-  annotations: {
+  labels: {
     text: {
-      fill: "#333333",
-      outlineWidth: 2,
-      outlineColor: "#ffffff",
-      outlineOpacity: 1,
+      fill: "#f3f4f6",
     },
-    link: {
-      stroke: "#000000",
-      strokeWidth: 1,
-      outlineWidth: 2,
-      outlineColor: "#ffffff",
-      outlineOpacity: 1,
-    },
-    outline: {
-      stroke: "#000000",
-      strokeWidth: 2,
-      outlineWidth: 2,
-      outlineColor: "#ffffff",
-      outlineOpacity: 1,
-    },
-    symbol: {
-      fill: "#000000",
-      outlineWidth: 2,
-      outlineColor: "#ffffff",
-      outlineOpacity: 1,
-    },
-  },
-  tooltip: {
-    container: {
-      background: "#ffffff",
-      color: "#333333",
-    },
-    basic: {},
-    chip: {},
-    table: {},
-    tableCell: {},
-    tableCellValue: {},
   },
 };
