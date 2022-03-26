469dc5a4744dc79ce83580e44e7abecbabffa429
chris0610 <chrislow360@gmail.com>
2022-03-26 12:33:12 -0400

    add gender waffle data

---
+++ src/data/gender.js
@@ -0,0 +1,7 @@
+export const genderData = [
+  { id: "Female", label: "Female", value: 68 },
+  { id: "Male", label: "Male", value: 535 },
+  { id: "Other", label: "Other", value: 2 },
+];
+
+export const total = 605;
