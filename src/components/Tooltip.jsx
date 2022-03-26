be445541adbe41f578d9a61e0562587563ce25f2
chris0610 <chrislow360@gmail.com>
2022-03-26 11:33:17 -0400

    add tooltip component

---
+++ src/components/Tooltip.jsx
@@ -0,0 +1,12 @@
+export default function Tooltip({ color, label, value }) {
+  return (
+    <div className="px-[9px] py-[5px] flex items bg-white text-black rounded-[2px]">
+      {color && (
+        <div className="w-3 h-3 mr-[7px]" style={{ backgroundColor: color }} />
+      )}
+      <p>
+        {label}: <span className="font-bold">{value}</span>
+      </p>
+    </div>
+  );
+}
