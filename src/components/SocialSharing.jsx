2fb9ae024045ae3affe3d3f4af3786a889dfda41
chris0610 <chrislow360@gmail.com>
2022-04-04 15:27:09 -0400

    increase z-index for social sharing modal overlay

--- src/components/SocialSharing.jsx
+++ src/components/SocialSharing.jsx
@@ -32,7 +32,7 @@ export default function SocialSharing({ path }) {
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog
           as="div"
-          className="fixed inset-0 z-10 overflow-y-auto bg-black bg-opacity-50"
+          className="fixed inset-0 z-50 overflow-y-auto bg-black bg-opacity-50"
           onClose={closeModal}
         >
           <div className="min-h-screen px-4 text-center">
